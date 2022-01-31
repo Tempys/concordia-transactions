64294a982b44b5fd5fe594c339b52a843891ae1f
Mykhailo <mikhail.dubik@gmail.com>
2022-01-31 21:08:10 +0200

    add go install script

--- scripts/go_install.sh
+++ scripts/go_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
+sudo apt-get update
 # First remove any existing old Go installation
 sudo rm -rf /usr/local/go
 
 # Install correct Go version
-curl https://dl.google.com/go/go1.17.2.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf -
+
+curl https://dl.google.com/go/go1.17.2.linux-amd64.tar.gz | sudo tar -C /usr/local -zxvf -
 
 # Update environment variables to include go
 cat <<'EOF' >>$HOME/.profile
@@ -15,8 +18,6 @@ export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 EOF
 
 source $HOME/.profile
-
-
 go version
 go version go1.17.2 linux/amd64
 
